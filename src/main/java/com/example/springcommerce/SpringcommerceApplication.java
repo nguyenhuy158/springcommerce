5b7b7a94337f3b5a4080dad5cd42bae3e0dc0e91
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-04-01 00:18:39 +0700

    first commit

---
+++ src/main/java/com/example/springcommerce/SpringcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/springcommerce/model/Product.java
@@ -0,0 +1,5 @@
+package com.example.springcommerce.model;
+
+public class Product {
+
+}
