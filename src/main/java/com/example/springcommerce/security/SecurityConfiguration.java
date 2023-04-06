2eb4eaa90bcd4b38a09681fdf3269d2ecc913458
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-04-07 00:35:55 +0700

    change ui

--- src/main/java/com/example/springcommerce/security/SecurityConfiguration.java
+++ src/main/java/com/example/springcommerce/security/SecurityConfiguration.java
@@ -27,11 +27,10 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
-        // http.authorizeHttpRequests().requestMatchers("/register",
-        // "/login").permitAll();
-
         http.authorizeHttpRequests(t -> t
                 .requestMatchers(HttpMethod.POST, "/login")
+                .permitAll()
+                .requestMatchers("/login?error")
                 .permitAll()
                 .requestMatchers("/register")
                 .permitAll())
@@ -47,38 +46,6 @@ public class SecurityConfiguration {
                         .permitAll())
                 .logout(logout -> logout.permitAll())
                 .exceptionHandling(handling -> handling.accessDeniedPage("/403"));
-
-        // http
-        // .authorizeHttpRequests()
-        // .requestMatchers(HttpMethod.POST, "/login").permitAll();
-
-        // http.authorizeHttpRequests().requestMatchers("/products").hasAnyRole(Role.USER.name(),
-        // Role.ADMIN.name());
-        // http.authorizeHttpRequests().requestMatchers("/users").hasRole(Role.ADMIN.name());
-
-        // http.authorizeHttpRequests().and().exceptionHandling(handling ->
-        // handling.accessDeniedPage("/403"));
-
-        // http.authorizeHttpRequests().and().formLogin(
-        // login -> login.loginProcessingUrl("/j_spring_security_check")
-        // .loginPage("/login")
-        // .defaultSuccessUrl("/products")
-        // .failureUrl("/login?error=True")
-        // .usernameParameter("username")
-        // .passwordParameter("password"))
-        // .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/"));
-
-        // http.authorizeHttpRequests().requestMatchers("/", "/sign-up", "/img/**",
-        // "/css/**", "/js/**")
-        // .permitAll()
-        // .anyRequest().authenticated() //
-        // .and()
-        // .formLogin(login ->
-        // login.loginPage("/login").defaultSuccessUrl("/home").failureUrl("/login?fail")
-        // .permitAll())
-        // .logout(logout ->
-        // logout.logoutSuccessUrl("/home").invalidateHttpSession(true)
-        // .deleteCookies("JSESSIONID").permitAll());
 
         return http.build();
     }
